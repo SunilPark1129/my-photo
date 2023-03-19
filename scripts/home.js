2d0917e6f177d6bfc326a5d5a830b866dd34b152
Sunil Park <sunilpark1129@gmail.com>
2023-03-19 12:29:03 -0400

    Edited Semantic Tags

--- scripts/home.js
+++ scripts/home.js
@@ -3,168 +3,167 @@ Feature: Slideshow
 Feature to slide sideways by pressing a button on the banner
 */
 
-const prevButton = document.querySelector('.slideshow__button--prev');
-const nextButton = document.querySelector('.slideshow__button--next');
-const slideshowTextH2 = document.querySelectorAll('.slideshow__text h2');
-const slideshowTextP = document.querySelectorAll('.slideshow__text p');
-const slideshowImage = document.getElementsByClassName('slideshow__image');
-const slideshowDot = document.getElementsByClassName('slideshow__dot');
+const prevButton = document.querySelector(".slideshow__button--prev");
+const nextButton = document.querySelector(".slideshow__button--next");
+const slideshowTextH4 = document.querySelectorAll(".slideshow__image h4");
+const slideshowTextP = document.querySelectorAll(".slideshow__image p");
+const slideshowImage = document.getElementsByClassName("slideshow__image");
+const slideshowDot = document.getElementsByClassName("slideshow__dot");
 
 let leftTrue = true,
-    rightTrue = false;
+  rightTrue = false;
 let leftIndex = slideshowImage.length - 1,
-    rightIndex = 1;
+  rightIndex = 1;
 let timer;
 let currentIndex = 0;
 
 slideMain();
 
 function slideMain() {
-    slideTimer();
-    slideClear();
-    slideCalc();
-    slideLeftRight(leftIndex, rightIndex);
-    slideOutput(currentIndex);
+  slideTimer();
+  slideClear();
+  slideCalc();
+  slideLeftRight(leftIndex, rightIndex);
+  slideOutput(currentIndex);
 }
 
+prevButton.addEventListener("click", () => {
+  buttonDisabled();
+  currentIndex -= 1;
+  leftTrue = true;
+  rightTrue = false;
 
-prevButton.addEventListener('click', () => {
-    buttonDisabled();
-    currentIndex -= 1;
-    leftTrue = true;
-    rightTrue = false;
-
-    leftIndex = currentIndex - 1;
-    rightIndex = currentIndex + 1;
-    slideMain();
+  leftIndex = currentIndex - 1;
+  rightIndex = currentIndex + 1;
+  slideMain();
 });
 
-nextButton.addEventListener('click', () => {
-    buttonDisabled();
-    currentIndex += 1;
-    rightTrue = true;
-    leftTrue = false;
+nextButton.addEventListener("click", () => {
+  buttonDisabled();
+  currentIndex += 1;
+  rightTrue = true;
+  leftTrue = false;
 
-    leftIndex = currentIndex - 1;
-    rightIndex = currentIndex + 1;
-    slideMain();
+  leftIndex = currentIndex - 1;
+  rightIndex = currentIndex + 1;
+  slideMain();
 });
 
 // Slide function with left or right button
 function slideLeftRight(l, r) {
-    if (l < 0) {
-        l = slideshowImage.length - 1;
-    }
-    if (r > slideshowImage.length - 1) {
-        r = 0;
-    }
+  if (l < 0) {
+    l = slideshowImage.length - 1;
+  }
+  if (r > slideshowImage.length - 1) {
+    r = 0;
+  }
 
-    if (leftTrue) {
-        slideshowImage[r].style.zIndex = '3';
-    }
-    if (rightTrue) {
-        slideshowImage[l].style.zIndex = '3';
-    }
-    slideshowImage[currentIndex].style.zIndex = '4';
+  if (leftTrue) {
+    slideshowImage[r].style.zIndex = "3";
+  }
+  if (rightTrue) {
+    slideshowImage[l].style.zIndex = "3";
+  }
+  slideshowImage[currentIndex].style.zIndex = "4";
 }
 
 // Initialize each class name and properties
 function slideClear() {
-    for (let i = 0; i < slideshowImage.length; i++) {
-        slideshowTextH2[i].classList.remove('slideshow__text--animation-0');
-        slideshowTextP[i].classList.remove('slideshow__text--animation-1');
-        slideshowDot[i].classList.remove('slideshow__dot--active');
+  for (let i = 0; i < slideshowImage.length; i++) {
+    slideshowTextH4[i].classList.remove("slideshow__text--animation-0");
+    slideshowTextP[i].classList.remove("slideshow__text--animation-1");
+    slideshowDot[i].classList.remove("slideshow__dot--active");
 
-        slideshowImage[i].style.zIndex = '1';
-        slideshowImage[i].style.left = '0%';
-    }
+    slideshowImage[i].style.zIndex = "1";
+    slideshowImage[i].style.left = "0%";
+  }
 }
 
 // Calculate when the slide reaches the end and link to the next slide
 function slideCalc() {
-    if (currentIndex < 0) {
-        currentIndex = slideshowImage.length - 1;
-    }
-    if (currentIndex > slideshowImage.length - 1) {
-        currentIndex = 0;
-    }
+  if (currentIndex < 0) {
+    currentIndex = slideshowImage.length - 1;
+  }
+  if (currentIndex > slideshowImage.length - 1) {
+    currentIndex = 0;
+  }
 }
 
 // Set the position value of the selected element to display on the screen
 function slideOutput(currentIndex) {
-    for (let i = 0; i < slideshowImage.length; i++) {
-        if (currentIndex < i) {
-            slideshowImage[i].style.left = '100%';
-            if (currentIndex == 0) {
-                slideshowImage[slideshowImage.length - 1].style.left = '-100%';
-            }
-        } else if (currentIndex > i) {
-            slideshowImage[i].style.left = '-100%';
-            if (currentIndex == slideshowImage.length - 1) {
-                slideshowImage[0].style.left = '100%';
-            }
-        }
+  for (let i = 0; i < slideshowImage.length; i++) {
+    if (currentIndex < i) {
+      slideshowImage[i].style.left = "100%";
+      if (currentIndex == 0) {
+        slideshowImage[slideshowImage.length - 1].style.left = "-100%";
+      }
+    } else if (currentIndex > i) {
+      slideshowImage[i].style.left = "-100%";
+      if (currentIndex == slideshowImage.length - 1) {
+        slideshowImage[0].style.left = "100%";
+      }
     }
+  }
 
-    slideshowTextH2[currentIndex].classList.add('slideshow__text--animation-0');
-    slideshowTextP[currentIndex].classList.add('slideshow__text--animation-1');
-    slideshowDot[currentIndex].classList.add('slideshow__dot--active');
+  slideshowTextH4[currentIndex].classList.add("slideshow__text--animation-0");
+  slideshowTextP[currentIndex].classList.add("slideshow__text--animation-1");
+  slideshowDot[currentIndex].classList.add("slideshow__dot--active");
 }
 
 // The dot is also given the ability to slide
 for (let i = 0; i < slideshowDot.length; i++) {
-    ((i) => {
-        slideshowDot[i].onclick = () => {
-            leftIndex = 0;
-            rightIndex = 0;
-            if (currentIndex < i) {
-                rightTrue = true;
-                leftTrue = false;
-                leftIndex = currentIndex;
-            } else if (currentIndex > i) {
-                rightTrue = false;
-                leftTrue = true;
-                rightIndex = currentIndex;
-            }
-            currentIndex = i;
-            buttonDisabled();
-            slideMain();
-        }
-    })(i);
+  ((i) => {
+    slideshowDot[i].onclick = () => {
+      leftIndex = 0;
+      rightIndex = 0;
+      if (currentIndex < i) {
+        rightTrue = true;
+        leftTrue = false;
+        leftIndex = currentIndex;
+      } else if (currentIndex > i) {
+        rightTrue = false;
+        leftTrue = true;
+        rightIndex = currentIndex;
+      }
+      currentIndex = i;
+      buttonDisabled();
+      slideMain();
+    };
+  })(i);
 }
 
 // Automatically slides over time.
 function slideTimer() {
-    clearInterval(timer);
-    timer = setInterval(() => {
-        buttonDisabled();
+  clearInterval(timer);
+  timer = setInterval(() => {
+    buttonDisabled();
 
-        currentIndex += 1;
+    currentIndex += 1;
 
-        leftTrue = false;
-        rightTrue = true;
+    leftTrue = false;
+    rightTrue = true;
 
-        leftIndex = currentIndex - 1;
-        rightIndex = currentIndex + 1;
+    leftIndex = currentIndex - 1;
+    rightIndex = currentIndex + 1;
 
-        slideMain();
-    }, 7000);
+    slideMain();
+  }, 7000);
 }
 
 // Avoid consecutive clicks on buttons
 function buttonDisabled() {
+  for (let i = 0; i < slideshowDot.length; i++) {
+    slideshowDot[i].disabled = true;
+  }
+  prevButton.disabled = true;
+  nextButton.disabled = true;
+  setTimeout(function () {
+    prevButton.disabled = false;
+    nextButton.disabled = false;
     for (let i = 0; i < slideshowDot.length; i++) {
-        slideshowDot[i].disabled = true;
+      slideshowDot[i].disabled = false;
     }
-    prevButton.disabled = true;
-    nextButton.disabled = true;
-    setTimeout(function() {
-        prevButton.disabled = false;
-        nextButton.disabled = false;
-        for (let i = 0; i < slideshowDot.length; i++) {
-            slideshowDot[i].disabled = false;
-        }
-    }, 1000);
+  }, 1000);
 }
 
 /*
@@ -172,41 +171,41 @@ Feature: Scroll Event (Navbar and About)
 Set the event function according to the Y scroll position
 */
 
-const navbar = document.getElementById('navbar');
-const navbarImg = document.querySelector('#navbar img');
-const aboutImage = document.getElementsByClassName('about__image');
-const aboutImageBox = document.getElementsByClassName('about__image-box');
-const aboutText = document.getElementsByClassName('about__text');
+const navbar = document.getElementById("navbar");
+const navbarImg = document.querySelector("#navbar img");
+const aboutImage = document.getElementsByClassName("about__image");
+const aboutImageBox = document.getElementsByClassName("about__image-box");
+const aboutText = document.getElementsByClassName("about__text");
 
-window.addEventListener('scroll', function() {
-    let docElem = document.documentElement;
-    let scrollPos = docElem.scrollTop;
+window.addEventListener("scroll", function () {
+  let docElem = document.documentElement;
+  let scrollPos = docElem.scrollTop;
 
-    // Navbar becomes sticky and follows the screen
-    if (scrollPos > 20) {
-        navbar.classList.add('nav--sticky');
-        navbarImg.style.width = '4rem';
-    } else {
-        navbar.classList.remove('nav--sticky');
-        navbarImg.style.width = '5rem';
-    }
+  // Navbar becomes sticky and follows the screen
+  if (scrollPos > 20) {
+    navbar.classList.add("nav--sticky");
+  } else {
+    navbar.classList.remove("nav--sticky");
+  }
 
-    // The picture in About moves in 3D with the mouse position
-    let CalcPos = scrollPos * .015;
-    let CalcPos2 = scrollPos * -.015;
+  // The picture in About moves in 3D with the mouse position
+  let CalcPos = scrollPos * 0.015;
+  let CalcPos2 = scrollPos * -0.015;
 
-    if (scrollPos < 1500) {
-        aboutImage[0].style.transform = `rotateY(20deg) rotateX(${CalcPos - 8}deg)`;
-        aboutImage[1].style.transform = `rotateY(-10deg) rotateX(${CalcPos2 + 20}deg)`;
-    }
+  if (scrollPos < 1500) {
+    aboutImage[0].style.transform = `rotateY(20deg) rotateX(${CalcPos - 8}deg)`;
+    aboutImage[1].style.transform = `rotateY(-10deg) rotateX(${
+      CalcPos2 + 20
+    }deg)`;
+  }
 });
 
 // When user clicks on a photo in about section, set the toggle to hide the text and resize the photo
 for (let i = 0; i < aboutImage.length; i++) {
-    ((i) => {
-        aboutImage[i].onclick = () => {
-            aboutImageBox[i].classList.toggle('about__image-box--active');
-            aboutText[i].classList.toggle('about__text--animation');
-        }
-    })(i);
+  ((i) => {
+    aboutImage[i].onclick = () => {
+      aboutImageBox[i].classList.toggle("about__image-box--active");
+      aboutText[i].classList.toggle("about__text--animation");
+    };
+  })(i);
 }
--- scripts/service.js
+++ scripts/service.js
@@ -3,44 +3,49 @@ Feature: Side Navigation
 Set the shortcut function on the side to move to the selected section
 */
 
-const navbar = document.getElementById('navbar');
-const navbarImg = document.querySelector('#navbar img');
-const sideNav = document.querySelector('.side-nav');
-const sideNavClose = document.querySelector('.side-nav__close');
+const navbar = document.getElementById("navbar");
+const navbarImg = document.querySelector("#navbar img");
+const sideNav = document.querySelector(".side-nav");
+const sideNavClose = document.querySelector(".side-nav__close");
 let sideNavTimer;
 
-window.addEventListener('scroll', () => {
-    let docElem = document.documentElement;
-    let scrollPos = docElem.scrollTop;
+window.addEventListener("scroll", () => {
+  let docElem = document.documentElement;
+  let scrollPos = docElem.scrollTop;
 
-    if (scrollPos > 20) {
-        navbarImg.style.width = '4rem';
-    } else {
-        navbarImg.style.width = '5rem';
-    }
+  // Navbar becomes sticky and follows the screen
+  if (scrollPos > 20) {
+    navbar.classList.add("nav--sticky");
+  } else {
+    navbar.classList.remove("nav--sticky");
+  }
 
-    // Activate the side nav whenever the user scrolls
-    sideNav.classList.add('side-nav--active');
+  // Activate the side nav whenever the user scrolls
+  sideNav.classList.add("side-nav--active");
 
-    // Reset all time settings
-    clearInterval(sideNavTimer);
+  // Reset all time settings
+  clearInterval(sideNavTimer);
 
-    // If the scroll value does not change after scrolling, it automatically disappears after 3 seconds
-    sideNavTimer = setInterval(() => { sideNav.classList.remove('side-nav--active') }, 3000);
+  // If the scroll value does not change after scrolling, it automatically disappears after 3 seconds
+  sideNavTimer = setInterval(() => {
+    sideNav.classList.remove("side-nav--active");
+  }, 3000);
 });
 
 sideNavClose.onclick = () => {
-    clearInterval(sideNavTimer);
-    sideNav.classList.remove('side-nav--active');
-}
+  clearInterval(sideNavTimer);
+  sideNav.classList.remove("side-nav--active");
+};
 
-sideNav.addEventListener('mouseover', () => {
-    clearInterval(sideNavTimer);
+sideNav.addEventListener("mouseover", () => {
+  clearInterval(sideNavTimer);
 });
 
-sideNav.addEventListener('mouseout', () => {
-    clearInterval(sideNavTimer);
-    sideNavTimer = setInterval(() => { sideNav.classList.remove('side-nav--active') }, 3000);
+sideNav.addEventListener("mouseout", () => {
+  clearInterval(sideNavTimer);
+  sideNavTimer = setInterval(() => {
+    sideNav.classList.remove("side-nav--active");
+  }, 3000);
 });
 
 /*
@@ -48,105 +53,105 @@ Feature: Photo Editor (Final Edit Section)
 Users can directly check what functions are provided by adding Photoshop functions to the photo
 */
 
-const finalEditDots = document.getElementsByClassName('final-edit__dots');
-const finalImageBox = document.querySelector('.final-edit__image-box');
-const finalOverlap = document.querySelectorAll('.final-edit__image-box > div');
-const finalImg = document.querySelector('.final-edit__image');
-const finalButton = document.querySelector('.final-edit__button');
-const finalSwitch1 = document.querySelector('.final-edit__switch__on');
-const finalSwitch2 = document.querySelector('.final-edit__switch__off');
+const finalEditDots = document.getElementsByClassName("final-edit__dots");
+const finalImageBox = document.querySelector(".final-edit__image-box");
+const finalOverlap = document.querySelectorAll(".final-edit__image-box > div");
+const finalImg = document.querySelector(".final-edit__image");
+const finalButton = document.querySelector(".final-edit__button");
+const finalSwitch1 = document.querySelector(".final-edit__switch__on");
+const finalSwitch2 = document.querySelector(".final-edit__switch__off");
 
 let previewDotIndex = 0,
-    leftDotIndex = 0,
-    rightDotIndex = 0,
-    leftDot = 2,
-    rightDot = 1;
+  leftDotIndex = 0,
+  rightDotIndex = 0,
+  leftDot = 2,
+  rightDot = 1;
 
 mainDot(0);
 
 // Function operated by pressing the color button
 for (let i = 0; i < finalEditDots.length; i++) {
-    ((i) => {
-        finalEditDots[i].onclick = () => {
-            removeAll();
-            searchDot(i);
-            zIndexDot(i);
-            mainDot(i);
-        }
-    })(i);
+  ((i) => {
+    finalEditDots[i].onclick = () => {
+      removeAll();
+      searchDot(i);
+      zIndexDot(i);
+      mainDot(i);
+    };
+  })(i);
 }
 
 // Reset all settings
 function removeAll() {
-    for (let i = 0; i < finalEditDots.length; i++) {
-        finalOverlap[i].classList.remove('final-edit__image--fade');
-    }
+  for (let i = 0; i < finalEditDots.length; i++) {
+    finalOverlap[i].classList.remove("final-edit__image--fade");
+  }
 }
 
 // Find the position of the button the user clicked and set which color would be left and right.
 function searchDot(n) {
-    leftDot = 2;
-    rightDot = 1;
-    if (n == 1) {
-        leftDot = 0;
-        rightDot = 2;
-    }
-    if (n == 2) {
-        leftDot = 1;
-        rightDot = 0;
-    }
+  leftDot = 2;
+  rightDot = 1;
+  if (n == 1) {
+    leftDot = 0;
+    rightDot = 2;
+  }
+  if (n == 2) {
+    leftDot = 1;
+    rightDot = 0;
+  }
 }
 
 // Put the z-index to determine the color to be in front and the color to be moved to the back to display animation effect
 function zIndexDot(n) {
-    if (previewDotIndex < n) {
-        leftDotIndex = 2;
-        rightDotIndex = 1;
-        if (previewDotIndex == 0 && n == 2) {
-            leftDotIndex = 1;
-            rightDotIndex = 2;
-        }
+  if (previewDotIndex < n) {
+    leftDotIndex = 2;
+    rightDotIndex = 1;
+    if (previewDotIndex == 0 && n == 2) {
+      leftDotIndex = 1;
+      rightDotIndex = 2;
     }
-    if (previewDotIndex > n) {
-        leftDotIndex = 1;
-        rightDotIndex = 2;
-        if (previewDotIndex == 2 && n == 0) {
-            leftDotIndex = 2;
-            rightDotIndex = 1;
-        }
+  }
+  if (previewDotIndex > n) {
+    leftDotIndex = 1;
+    rightDotIndex = 2;
+    if (previewDotIndex == 2 && n == 0) {
+      leftDotIndex = 2;
+      rightDotIndex = 1;
     }
-    previewDotIndex = n;
+  }
+  previewDotIndex = n;
 
-    finalEditDots[leftDot].style.zIndex = leftDotIndex;
-    finalEditDots[rightDot].style.zIndex = rightDotIndex;
+  finalEditDots[leftDot].style.zIndex = leftDotIndex;
+  finalEditDots[rightDot].style.zIndex = rightDotIndex;
 }
 
 // adds a transform to display 3d effect
 function mainDot(n) {
-    finalOverlap[n].classList.add('final-edit__image--fade');
+  finalOverlap[n].classList.add("final-edit__image--fade");
 
-    finalEditDots[n].style.zIndex = 5;
+  finalEditDots[n].style.zIndex = 5;
 
-    finalEditDots[n].style.left = '50%';
-    finalEditDots[n].style.transform = 'translateX(-50%) scale(1.5)';
+  finalEditDots[n].style.left = "50%";
+  finalEditDots[n].style.transform = "translateX(-50%) scale(1.5)";
 
-    finalEditDots[leftDot].style.left = '0%';
-    finalEditDots[leftDot].style.transform = 'none';
+  finalEditDots[leftDot].style.left = "0%";
+  finalEditDots[leftDot].style.transform = "none";
 
-    finalEditDots[rightDot].style.left = '100%';
-    finalEditDots[rightDot].style.transform = 'translateX(-100%)';
+  finalEditDots[rightDot].style.left = "100%";
+  finalEditDots[rightDot].style.transform = "translateX(-100%)";
 }
 
 // Operated when the filter button is pressed
 finalButton.onclick = () => {
-    finalImg.classList.toggle('final-edit__image--filter');
-    finalSwitch1.classList.toggle('final-edit__switch--on');
-    finalSwitch2.classList.toggle('final-edit__switch--off');
-}
+  finalImg.classList.toggle("final-edit__image--filter");
+  finalSwitch1.classList.toggle("final-edit__switch--on");
+  finalSwitch2.classList.toggle("final-edit__switch--off");
+};
 
 // Operated when the picture is pressed
 finalImageBox.onclick = () => {
-    for (let i = 0; i < finalOverlap.length; i++) {
-        finalOverlap[i].classList.toggle('final-edit__image-zoom');
-    }
-}
+  for (let i = 0; i < finalOverlap.length; i++) {
+    finalOverlap[i].classList.toggle("final-edit__image-zoom");
+  }
+};
